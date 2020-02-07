854748a8c859579779a351207af752a98d8419e9
Ahmad tlz <dbag201@yahoo.com>
2020-02-07 07:46:07 +0330

    Updates

---
+++ precache-manifest.38075da95b1f549b6e70941f5349d5b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5783f58f95800f1cdf825794517f3966",
+    "url": "/ahmadTlz/index.html"
+  },
+  {
+    "revision": "f56337b6f31421b72f81",
+    "url": "/ahmadTlz/static/css/main.d0b21fe3.chunk.css"
+  },
+  {
+    "revision": "bfb99b2d841c63fd3b72",
+    "url": "/ahmadTlz/static/js/2.e127e8d7.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/ahmadTlz/static/js/2.e127e8d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f56337b6f31421b72f81",
+    "url": "/ahmadTlz/static/js/main.9bda2d4d.chunk.js"
+  },
+  {
+    "revision": "850a83440d62d8a22e1b",
+    "url": "/ahmadTlz/static/js/runtime-main.14482b1f.js"
+  }
+]);
